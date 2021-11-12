e61e0d5ba08e7db6ccedc94ba17759063203eb65
robin <robin@ansvia.com>
2021-11-12 19:03:03 +0700

    handle .mjs correctly during build.

--- frontends/web/nuxt.config.js
+++ frontends/web/nuxt.config.js
@@ -38,5 +38,13 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {
+    extend(config, { isDev, isClient }) {
+      config.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      });
+    }
+  }
 };
